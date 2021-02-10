dfddfad8a978d37d0bb9dc0385be2781ea9c3af3
Ahn YoungSeon <ys.ahn@outlook.com>
2021-02-10 22:32:05 +0900

    load model

--- kobert_transformers/load_model.py
+++ kobert_transformers/load_model.py
@@ -1,25 +1,37 @@
 from transformers import BertModel, BertForMaskedLM, DistilBertModel, DistilBertForMaskedLM
 
 
-def get_kobert_model():
+def get_kobert_model(cache_dir=None):
     """ Return BertModel for Kobert """
-    model = BertModel.from_pretrained('monologg/kobert')
+    if cache_dir is not None:
+        model = BertModel.from_pretrained(cache_dir)
+    else:
+        model = BertModel.from_pretrained('monologg/kobert')
     return model
 
 
-def get_kobert_lm():
+def get_kobert_lm(cache_dir=None):
     """ Return BertForMaskedLM for Kobert """
-    model = BertForMaskedLM.from_pretrained('monologg/kobert-lm')
+    if cache_dir is not None:
+        model = BertModel.from_pretrained(cache_dir)
+    else:
+        model = BertForMaskedLM.from_pretrained('monologg/kobert-lm')
     return model
 
 
-def get_distilkobert_model():
+def get_distilkobert_model(cache_dir=None):
     """ Return DistilBertModel for DistilKobert """
-    model = DistilBertModel.from_pretrained('monologg/distilkobert')
+    if cache_dir is not None:
+        model = BertModel.from_pretrained(cache_dir)
+    else:
+        model = DistilBertModel.from_pretrained('monologg/distilkobert')
     return model
 
 
-def get_distilkobert_lm():
+def get_distilkobert_lm(cache_dir=None):
     """ Return DistilBertForMaskedLM for DistilKobert """
-    model = DistilBertForMaskedLM.from_pretrained('monologg/distilkobert')
+    if cache_dir is not None:
+        model = BertModel.from_pretrained(cache_dir)
+    else:
+        model = DistilBertForMaskedLM.from_pretrained('monologg/distilkobert')
     return model
