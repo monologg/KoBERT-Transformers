3d5f699cc0bec228c7c8eeb261999ee5b3a97871
Ahn YoungSeon <ys.ahn@outlook.com>
2021-02-10 22:25:42 +0900

    load tokenizer

--- kobert_transformers/utils.py
+++ kobert_transformers/utils.py
@@ -1,5 +1,8 @@
 from .tokenization_kobert import KoBertTokenizer
 
 
-def get_tokenizer():
-    return KoBertTokenizer.from_pretrained('monologg/kobert')
+def get_tokenizer(cache_dir=None):
+    if cache_dir is not None:
+        return KoBertTokenizer.from_pretrained(cache_dir)
+    else:
+        return KoBertTokenizer.from_pretrained('monologg/kobert')
