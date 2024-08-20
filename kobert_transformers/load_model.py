5ea5e2d535b14fff8e03d4a822c5f5eb7d04d0e1
monologg <adieujw@gmail.com>
2024-08-20 20:03:22 +0900

    style: lint

--- kobert_transformers/load_model.py
+++ kobert_transformers/load_model.py
@@ -2,24 +2,24 @@ from transformers import BertForMaskedLM, BertModel, DistilBertForMaskedLM, Dist
 
 
 def get_kobert_model():
-    """ Return BertModel for Kobert """
+    """Return BertModel for Kobert"""
     model = BertModel.from_pretrained("monologg/kobert")
     return model
 
 
 def get_kobert_lm():
-    """ Return BertForMaskedLM for Kobert """
+    """Return BertForMaskedLM for Kobert"""
     model = BertForMaskedLM.from_pretrained("monologg/kobert-lm")
     return model
 
 
 def get_distilkobert_model():
-    """ Return DistilBertModel for DistilKobert """
+    """Return DistilBertModel for DistilKobert"""
     model = DistilBertModel.from_pretrained("monologg/distilkobert")
     return model
 
 
 def get_distilkobert_lm():
-    """ Return DistilBertForMaskedLM for DistilKobert """
+    """Return DistilBertForMaskedLM for DistilKobert"""
     model = DistilBertForMaskedLM.from_pretrained("monologg/distilkobert")
     return model
